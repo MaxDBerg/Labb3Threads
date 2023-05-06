e8e6b45c1ba8bd0c93187088b035f41c4b6ce7fd
MaxDBerg <max.dahlberg@gmail.com>
2023-05-06 12:24:20 +0200

    Add project files.

---
+++ Labb3Threads/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Labb3Threads
+{
+    internal class Car
+    {
+        public string carName { get; set; }
+        public int carSpeed { get; set; }
+        public decimal distanceTraveled { get; set; }
+        public List<string> AccidentsLog { get; set; }
+        public int PenaltyTimer { get; set; }
+        private System.Timers.Timer eventTimer;
+        public Car(string _carName, int _carSpeed)
+        {
+            carName = _carName;
+            carSpeed = _carSpeed;
+            distanceTraveled = 0;
+            AccidentsLog = new List<string>();
+            PenaltyTimer = 0;
+
+            eventTimer = new System.Timers.Timer(3000);
+            // Set the event handler for the Elapsed event, which fires when the timer interval elapses
+            eventTimer.Elapsed += OnTimedEvent;
+            eventTimer.AutoReset = true;
+            // Start the timer
+            eventTimer.Start();
+        }
+
+        public void StartDriving()
+        {
+            while (distanceTraveled < 10000 && Program.raceOngoing)
+            {
+                // Move the car forward
+                distanceTraveled += carSpeed / 10;
+
+                Thread.Sleep(PenaltyTimer);
+                PenaltyTimer = 0;
+
+                Thread.Sleep(50);
+
+            }
+        }
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (new Random().Next(50) < 1)
+            {
+                AccidentsLog.Add("Car stopped to refuel");
+                PenaltyTimer = 3000; // 30 seconds to refuel
+            }
+            else if (new Random().Next(50) < 2)
+            {
+                AccidentsLog.Add("Car stopped to change a tire");
+
+                PenaltyTimer = 2000; // 20 seconds to change a tire
+
+            }
+            else if (new Random().Next(50) < 5)
+            {
+                AccidentsLog.Add("Car stopped to clean the windshield");
+
+                PenaltyTimer = 1000; // 10 seconds to clean the windshield
+
+            }
+            else if (new Random().Next(50) < 10)
+            {
+                AccidentsLog.Add("Car slowed down due to engine trouble");
+
+                carSpeed--; // decrease speed by 1 km/h
+
+            }
+        }
+    }
+}
---
+++ Labb3Threads/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Threading;
+
+namespace Labb3Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            RaceStart();
+
+        }
+
+        public static bool raceOngoing = true;
+        
+        public static void RaceStart()
+        {
+
+            List<Car> cars = new List<Car>();
+            List<Thread> threads = new List<Thread>();
+            int speed = 120;
+
+            Car Car1 = new Car("Volvo", speed);
+            Car Car2 = new Car("Ford", speed);
+
+            cars.Add(Car1);
+            cars.Add(Car2);
+
+            Thread thread1 = new Thread(() => Car1.StartDriving());
+            Thread thread2 = new Thread(() => Car2.StartDriving());
+
+            threads.Add(thread1);
+            threads.Add(thread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            // Get the initial cursor position
+            int initialTop = Console.CursorTop;
+
+            while (raceOngoing)
+            {
+                // Update the relevant lines of the console
+                Console.SetCursorPosition(0, initialTop);
+                DisplayDistances(cars);
+
+                Thread.Sleep(50);
+
+                // Check if any car has reached the finish line
+                if (cars.Any(c => c.distanceTraveled >= 10000))
+                {
+                    raceOngoing = false;
+                    Console.SetCursorPosition(0, initialTop);
+                    DisplayDistances(cars);
+                    break;
+                }
+
+            }
+
+            // Join all threads to ensure they have terminated
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            List<Car> sortedCarsByPlacing =  cars.OrderByDescending(c => c.distanceTraveled).ToList();
+
+            Console.WriteLine($"{sortedCarsByPlacing.First().carName} - Won the Race ^^ {sortedCarsByPlacing.First().distanceTraveled}");
+
+            Console.WriteLine("Race finished!");
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"Accidents Log for {car.carName}");
+                foreach (string log in car.AccidentsLog)
+                {
+                    Console.WriteLine(log);
+                } 
+            }
+
+            Console.ReadKey();
+
+        }
+        public static void DisplayDistances(List<Car> cars)
+        {
+            // Save the current cursor position
+            int left = Console.CursorLeft;
+            int top = Console.CursorTop;
+
+            // Sort the cars by distance in descending order
+            List<Car> sortedCars = cars.OrderByDescending(c => c.distanceTraveled).ToList();
+
+            // Display the distance of each car in order
+            for (int i = 0; i < sortedCars.Count; i++)
+            {
+                Console.SetCursorPosition(left, top + i);
+                Console.WriteLine($"[{i + 1}] - {sortedCars[i].carName} - Distans Traveled: {sortedCars[i].distanceTraveled}km Current Speed: {sortedCars[i].carSpeed}km/h                  ");
+            }
+        }
+
+    }
+}
